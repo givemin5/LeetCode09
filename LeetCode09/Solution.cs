4c92c7fd79a2542049a2000b1e07476b072ec19e
givemin5 <givemin5@gmail.com>
2018-02-19 21:42:31 +0800

    Leet Code 09

---
+++ LeetCode09/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode09
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            var input = x;
+            if (x < 0)
+                return false;
+            int anti = 0;
+            while (x != 0)
+            {
+                var t = x % 10;
+                x = (x - t) / 10;
+
+                anti = anti * 10 + t;
+
+            }
+
+            return anti == input || input  == anti/10;
+        }
+    }
+}
---
+++ LeetCode09Tests/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode09.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void IsPalindrome_n2147483648_ShouldBe_False()
+        {
+            int input = -2147483648;
+            Solution solution = new Solution();
+            Assert.AreEqual(false, solution.IsPalindrome(input));
+        }
+
+        [TestMethod()]
+        public void IsPalindrome_10_ShouldBe_False()
+        {
+            int input = 10;
+            Solution solution = new Solution();
+            Assert.AreEqual(false,solution.IsPalindrome(input));
+        }
+    }
+}
